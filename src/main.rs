13addd171cc5a0b55a829e33120c13a07c321215
Hyeon Kim <simnalamburt@gmail.com>
2024-06-19 22:50:44 +0900

    New routes: /spec{,.json,.yaml}

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,12 @@ async fn main() -> Result<(), std::io::Error> {
         OpenApiService::new(Api, "Hello, world!", "0.1").server("http://localhost:3000/api");
     let ui = api_service.swagger_ui();
 
-    let app = Route::new().nest("/api", api_service).nest("/ui", ui);
+    let app = Route::new()
+        .nest(r"/ui", ui)
+        // TODO: Publish spec only in development mode
+        .at(r"/<spec(?:\.yaml)?>", api_service.spec_endpoint_yaml())
+        .at(r"/spec.json", api_service.spec_endpoint())
+        .nest(r"/api", api_service);
 
     Server::new(TcpListener::bind("0.0.0.0:3000"))
         .run(app)
