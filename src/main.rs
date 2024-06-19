4b871598566c21d75360b6fd3d3393728754e547
Hyeon Kim <simnalamburt@gmail.com>
2024-06-19 22:25:20 +0900

    Hello, poem!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use poem::{get, handler, listener::TcpListener, web::Path, Route, Server};
+
+#[handler]
+fn hello(Path(name): Path<String>) -> String {
+    format!("hello: {}", name)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let app = Route::new().at("/hello/:name", get(hello));
+    Server::new(TcpListener::bind("0.0.0.0:3000"))
+      .run(app)
+      .await
 }
