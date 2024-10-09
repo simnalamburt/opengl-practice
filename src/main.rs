68401a7d0710ba08e37ffd962e8f225bd3559c97
Hyeon Kim <simnalamburt@gmail.com>
2024-10-10 04:42:23 +0900

    Hello, sqlx!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use dotenvy::EnvLoader;
+use sqlx::{postgres::PgPoolOptions, query};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let dotenv = EnvLoader::new().load()?;
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&dotenv.var("DATABASE_URL")?)
+        .await?;
+
+    let result = query!("SELECT * FROM auth.users").fetch_all(&pool).await?;
+
+    for row in result {
+        println!("{:?}", row.email);
+    }
+
+    Ok(())
 }
