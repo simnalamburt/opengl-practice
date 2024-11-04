af58ddbb2dfbb71d5917f37a543e1779e6ff8567
Hyeon Kim <simnalamburt@gmail.com>
2024-11-04 21:23:09 +0900

    Hello, SQLite3

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,11 @@
-use anyhow::Result;
-use dotenvy::EnvLoader;
-use sqlx::{postgres::PgPoolOptions, query};
+use sqlx::{query, sqlite::SqlitePool};
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let dotenv = EnvLoader::new().load()?;
-
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect(&dotenv.var("DATABASE_URL")?)
-        .await?;
-
-    let result = query!("SELECT * FROM auth.users").fetch_all(&pool).await?;
-
+async fn main() -> anyhow::Result<()> {
+    let pool = SqlitePool::connect("sqlite:database.sqlite3").await?;
+    let result = query!("SELECT * FROM person").fetch_all(&pool).await?;
     for row in result {
-        println!("{:?}", row.email);
+        println!("{:#?}", row);
     }
-
     Ok(())
 }
