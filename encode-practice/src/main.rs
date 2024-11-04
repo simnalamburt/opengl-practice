97698707b2db35f4c5ef8cf41cf9ae8dc49be300
Hyeon Kim <simnalamburt@gmail.com>
2024-11-04 20:18:29 +0900

    encode-practice: Update dependencies & codes

--- encode-practice/src/main.rs
+++ encode-practice/src/main.rs
@@ -1,40 +1,28 @@
 use obj::{load_obj, Obj};
-use std::fs::File;
-use std::io::BufReader;
-use time::Instant;
+use std::{fs::File, io::BufReader, time::Instant};
 
 fn main() {
     let input = BufReader::new(File::open("rilakkuma.obj").unwrap());
-    let t0 = Instant::now();
 
+    let t = Instant::now();
     let dome: Obj = load_obj(input).unwrap();
-    let t = Instant::now();
-    println!("obj 로딩 : {}ms", (t - t0).whole_milliseconds());
-    let t0 = t;
-
+    println!("obj 로딩 : {:?}", Instant::now() - t);
     println!();
 
+    let t = Instant::now();
     let encoded = serde_json::to_string(&dome).unwrap();
-    let t = Instant::now();
-    println!("serde_json 인코딩 : {}ms", (t - t0).whole_milliseconds());
-    let t0 = t;
+    println!("serde_json 인코딩 : {:?}", Instant::now() - t);
 
+    let t = Instant::now();
     let _decoded: Obj = serde_json::from_str(&encoded).unwrap();
-    let t = Instant::now();
-    println!("serde_json 디코딩 : {}ms", (t - t0).whole_milliseconds());
-    let t0 = t;
-
+    println!("serde_json 디코딩 : {:?}", Instant::now() - t);
     println!();
 
+    let t0 = Instant::now();
     let encoded = bincode::serialize(&dome).unwrap();
-    let t = Instant::now();
-    println!("bincode 인코딩 : {}ms", (t - t0).whole_milliseconds());
-    let t0 = t;
+    println!("bincode 인코딩 : {:?}", Instant::now() - t0);
 
+    let t0 = Instant::now();
     let _decoded: Obj = bincode::deserialize(&encoded).unwrap();
-    let t = Instant::now();
-    println!("bincode 디코딩 : {}ms", (t - t0).whole_milliseconds());
-    let t0 = t;
-
-    let _ = t0;
+    println!("bincode 디코딩 : {:?}", Instant::now() - t0);
 }
