356f6c0d4cbcbef6c0e6038c085db63294f8d0bb
Hyeon Kim <simnalamburt@gmail.com>
2024-11-04 21:08:49 +0900

    jwt-practice: Fix clippy warnings

--- jwt-practice/src/main.rs
+++ jwt-practice/src/main.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use jsonwebtoken::{decode, DecodingKey, Validation};
 use std::collections::HashMap;
 
-const TOKEN: &'static str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjIsImV4cCI6OTU3NTg3ODM1NywiYXVkIjoiZm9vIn0.lOd828g8wv0Fc8e44GzSn0vkOMGJR4PIbX5baJl9UnE";
+const TOKEN: &str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjIsImV4cCI6OTU3NTg3ODM1NywiYXVkIjoiZm9vIn0.lOd828g8wv0Fc8e44GzSn0vkOMGJR4PIbX5baJl9UnE";
 
 type Object = HashMap<String, serde_json::Value>;
 
